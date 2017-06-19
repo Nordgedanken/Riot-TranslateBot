85bb3dfceecaaabb72f1cc886d9b3578194042ae
MTRNord <mtrnord1@gmail.com>
2017-06-19 20:28:43 +0200

    Basic Idea of command listening (Not working)
    Signed-off-by: MTRNord <mtrnord1@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import login from './login';
 
-const new_login = new login;
-new_login.getClient().then(client => {
-    console.log("client: " + client);
-    client.publicRooms(function(err, data) {
-        console.log("Public Rooms: %s", JSON.stringify(data));
+const mx = new login;
+mx.getClient().then(client => {
+    client.on("event", function(event) {
+        console.log(event.getType());
     });
+    client.on("Room.timeline", (event, room, toStartOfTimeline) => {
+        console.log(event.getType());
+        if (toStartOfTimeline) {
+            return; // don't print paginated results
+        }
+        if (event.getType() !== "m.room.message") {
+            return; // only print messages
+        }
+        console.log(
+            // the room name will update with m.room.name events automatically
+            "(%s) %s :: %s", room.name, event.getSender(), event.getContent().body
+        );
+    });
+    client.startClient();
 });
 
